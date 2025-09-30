9a9afa7cc260e2c4438c523a28b46ce76dc84fa0
zannatulferdousianika <zfanika79@gmail.com>
2025-09-30 23:26:31 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,132 +1,150 @@
-import tkinter as tk 
-from tkinter import messagebox,ttk
-
-class RestaurantOrderManagement:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Restaurant Management App")
+import random
+import pygame
 
 
-        self.menu_item={
-            "FRIES MEAL":2,
-            "COMBO LUNCH": 3,
-            "BURGER MEAL": 2,
-            "SUBSANDWICH":3,
-            "PIZZA MEAL": 4,
-            "SPICY NAGA CHICKEN(2 PIECE)": 3,
-            "SALAD": 2,
-            "DRINKS": 1,
-            "SPICY CHOWMIN": 3,
-            "ICE CREAM": 2,
-            
-            "HOT DRINKS":0,
-            "COFFEE":1.5,
-            "CAPACHINO": 2,
-            "LATTE": 2.5,
-            "MATCHA TEA": 3,
-            "COLD CHOCOLATE COFFEE": 3.5,
-            "ICE AMERICANO":3
-
-        }
-
-        self.exchange_rate = 120
-
-        self.setup_background(root)
-
-        frame = ttk.Frame(root)
-        frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-        ttk.abel(frame, text="Restaurant Order Management", font=("Arial",29,"bold")).grid(row=0, columnspan=4,
-        padx=10, pady=10)
-
-        self.menu_labels = {}
-        self.menu_quantities ={}
-
-
-        ttk.label(frame, text="Item", font=("Arial", 10, "bold")).grid(row=1, column=0, padx=10)
-        ttk.label(frame, text="Price (USD)", font=("Arial", 10, "bold")).grid(row=1, column=1, padx=10)
-        ttk.label(frame, text="Price (BDT)", font=("Arial", 10, "bold")).grid(row=1, column=2, padx=10)
-        ttk.label(frame, text="Quantity", font=("Arial", 10, "bold")).grid(row=1, column=3, padx=10)
-
-
-        for i , (item, price) in enumerate(self.mneu_items.items(), start=2):
-            #The enumerate() function in Python is a built-in function that 
-            # adds a counter to an iterable object (like a list, tuple,
-            #  or string) and returns an enumerate object.
-            #  This object can then be used in a for loop to access both the index 
-            # and the value of each item in the iterable simultaneously.
-
-            bdt_price = price * self.exchange_rate
-            ttk.Label(frame, text=f"{item}:", font=("Arial", 12)).grid(row=i, column=0, padx=10, pady=5)
-            ttk.Label(frame, text=f"${price:.2f}:", font=("Arial", 12)).grid(row=i, column=1, padx=10, pady=5)
-            ttk.Label(frame, text=f"৳{bdt_price:.0f}:", font=("Arial", 12)).grid(row=i, column=2, padx=10, pady=5)
-
-            quantity_entry = ttk.Entry(frame, width=5)
-            quantity_entry.grid(row=1, column=3, padx=10, pady=5)
-            self.menu_quantities[item] = quantity_entry
+class Button():
+    def __init__(self, x, y, pos, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.pos = pos
         
-        order_button = ttk.Button(frame, text="Place Order", command=self.place_order)
+    def clicked(self, pos):
+        self.pos = pygame.mouse.get_pos()
+        if self.pos[0] > self.x and self.pos[0] < self.x + self.width:
+            if self.pos[1] > self.y and self.pos[1] < self.y + self.height:
+                return True
+        return False
+    
+class RpsGame():
+    def __init__(self):
+        pygame.init()
 
-        order_button.grid(row=len(self.menu_items)+2, column=0, columnspan=2, padx=10, pady=10)
+        self.screen = pygame.display.set_mode((960, 640))
+        pygame.display.set_caption("Rock Paper Scissior")
 
+        self.bg = pygame.image.load("background.jpg")
+        self.r_btn = pygame.image.load("r_button.png").convert_alpha()
+        self.p_btn = pygame.image.load("p_button.png").convert_alpha()
+        self.s_btn = pygame.image.load("s_button.png").convert_alpha()
 
-        clear_button = ttk.Button(frame, text="Clear Order", command=self.clear_order)
-        clear_button.grid(row=len(self.menu_items)+2, column=2, columnspan=2, padx=10, pady=10)
-                
+        self.choose_rock = pygame.image.load("rock.png").convert_alpha()
+        self.choose_paper = pygame.image.load("paper.png").convert_alpha()
+        self.choose_scissors = pygame.image.load("scissors.png").convert_alpha()
 
-        for entry in self.menu_quantities.values():
-            entry.bind("<KeyRelease>", self.update_total)
-    def setup_background(self, root):
-        bg_width, bg_height = 800,600
-        canvas = tk.Canvas(root, width=bg_width, height=bg_height)
-        canvas.pack()
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.r_btn, (20, 500))
+        self.screen.blit(self.p_btn, (330, 500))
+        self.screen.blit(self.s_btn, (640, 500))
 
-        try:
-            original_image = tk.PhotoImage(file="background_png")
-            background_image = original_image.subsample(
-                max(1, original_image.width() // bg_width),
-                max(1, original_image.height() // bg_height))
-            canvas.create_image(,,anchor=tk.NW, image+background_image)
-            canvas.image = background_image
-        except Exception:
+        self.rock_btn = Button(30, 520, (30, 520), 300, 140)
+        self.paper_btn = Button(340, 520, (340, 520), 300, 140)
+        self.scissors_btn = Button(640, 520, (640, 520), 300, 140)
+
+        self.font = pygame.font.Font(('Splatch.ttf'), 90)
+        self.text = self.font.render(f" ", True, (255, 255, 255))
+
+        self.pl_score = 0
+        self.pc_score = 0
+
+    def player(self):
+        if self.rock_btn.clicked(30):
+            self.p_option = "rock"
+            self.screen.blit(self.choose_rock, (120, 200))
+        elif self.paper_btn.clicked(340):
+            self.p_option = "paper"
+            self.screen.blit(self.choose_paper, (120, 200))
+        else:
+            self.scissors_btn.clicked(640)
+            self.p_option = "scissors"
+            self.screen.blit(self.choose_scissors, (120, 200))
+
+        return self.p_option
+
+    def computer(self):
+        self.pc_random_choice = " "
+        option = ["rock", "paper", "scissors"]
+        pc_choice = random.choice(list(option))
+        if pc_choice == "rock":
+            self.pc_random_choice = "rock"
+            pc_choice = self.choose_rock
+        elif pc_choice == "paper":
+            self.pc_random_choice = "paper"
+            pc_choice = self.choose_paper
+        else:
+            self.pc_random_choice = "scissors"
+            pc_choice = self.choose_scissors
+        pc_option = self.screen.blit(pc_choice, (600, 200))
+        return pc_option
+
+    def pl_score_cache(self):
+        self.pl_score = 0
+        self.pc_score = 0
+
+        pl = self.p_option
+        pc = self.pc_random_choice
+
+        if pl == "rock" and pc == "paper" or pl == "paper" and pc == "scissors" or pl == "scissors" and pc == "rock":
+            self.pc_score += 1
+        elif pl == pc:
             pass
-    def place_order(self):
-        total_cost=0
-        order_summary = "Order Summary: \n"
-        symbol = "৳"
-        rate = self.exchange_rate
-        for item, entry in self.menu_quantities.items():
-            quantity = entry.get()
-            if quantity.isdigit():
-                quantity = int(quantity)
-                price = self.menu_items[item]*rate
-                cost = quantity*price
-                total_cost += cost 
-                if quantitt > 0:
-                    order_summary += f"{item}: {quantity} x {sumbol}{price:.0f} = {sumbol}{cost:.0f}\n"
-                    self.clear_order()
+        else:
+            self.pl_score += 1
 
-                if total_cost >0:
-                    order_summary += f"\nTotal Cost: {symbol}{total_cost:.0f}"
-                    messagebox.showinfo("Order Placed", order_summary)
+        return self.pl_score
 
-                else:
-                    messagebox.showerroe("Error", "Please order at least one item.")
+    def pc_score_cache(self):
+        self.pl_score = 0
+        self.pc_score = 0
 
-    def update_total(self, event=None):
-        total_cost = 0
-        rate = self.exchange_rate
-        for item, entry in self.menu_quantities.items():
-            quantity = entry.get()
-            if quantity.isdigit():
-                quantity = int(quantity)
-                price = self.menu_items[item]*rate
-                cost = quantity *price
-                total_cost += cost
-        self.total_var.set(f"Total: ৳{total_cost:.0f}")
+        pl = self.p_option
+        pc = self.pc_random_choice
 
-if __name__ =="__main__":
-    root = tk.Tk()
-    app = R estaurantOrderManagement(root)
-    root.geometry("800x600")
-    root.mainloop()
+        if pl == "rock" and pc == "paper" or pl == "paper" and pc == "scissors" or pl == "scissors" and pc == "rock":
+            self.pc_score += 1
+        elif pl == pc:
+            pass
+        else:
+            self.pl_score += 1
+        return self.pc_score
+
+    def image_reset(self):
+        self.screen.blit(self.text, (330, 0))
+        self.text = self.font.render(" ", True, (0, 0, 0))
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.r_btn, (20, 500))
+        self.screen.blit(self.p_btn, (330, 500))
+        self.screen.blit(self.s_btn, (640, 500))
+        pass
+
+    def game_loop(self):
+        run = True
+        clock = pygame.time.Clock()
+        rps_game = RpsGame()
+
+        while run:
+            pygame.display.update()
+            self.screen.blit(self.text, (330, 0))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.rock_btn.clicked(30) or self.paper_btn.clicked(340) or self.scissors_btn.clicked(640):
+                        rps_game.image_reset()
+                        rps_game.player()
+                        rps_game.computer()
+
+                        self.pl_score += rps_game.pl_score_cache()
+                        self.pc_score += rps_game.pc_score_cache()
+                        self.text = self.font.render(f"{self.pl_score} : {self.pc_score}", True, (255, 255, 255))
+      
+            pygame.display.flip()
+            clock.tick(30)
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = RpsGame()
+    game.game_loop()
